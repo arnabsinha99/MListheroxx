3f4c186a41cf4ea908766194daef65479e36071d
Arnab Sinha <sarnab433@gmail.com>
2019-02-07 20:28:34 +0530

    Update Sequential_model.py

--- keras/Sequential_model.py
+++ keras/Sequential_model.py
@@ -29,7 +29,7 @@ label= np.random.randint(2,size=(100,1))
 data_test = np.random.random((10,100))
 data_pred = np.random.randint(2,size = (10,1))
 
-model.fit(data,label,epochs = 10, batch_size = 10)
+model.fit(data_train,label,epochs = 10, batch_size = 10)
 
 score = model.evaluate(data_test,data_pred, batch_size = 10)
 
